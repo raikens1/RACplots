5a66ec1c7b1a68406f46812982a6c7e3294724ac
raikens1 <rockyaikens@gmail.com>
2021-02-05 11:03:59 -0800

    Finish debug of main figures after refactor

--- R/prop_histograms.R
+++ R/prop_histograms.R
@@ -7,7 +7,7 @@
 propensity_histogram <- function(data){
   ggplot(data, aes(x = prop, y=..density.., fill = t)) +
     geom_histogram(alpha = 0.4, position = "identity") +
-    scale_fill_brewer(palette = "Set1") +
+    scale_fill_brewer(palette = "Set1", direction = -1) +
     theme(legend.position = "none") +
     xlab(expression(paste("Propensity, ", phi, "(x)", sep = ""))) +
     ylab("Density")
